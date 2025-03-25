bb94f8d4abab10d7fe8a8f310c525a4586315542
joaoLouceiro <jrlouceiro@live.com>
2025-03-25 13:27:41 +0000

    Day 41: Flight Club Email

--- day_40_CAPSTONE_flight_club/data_manager.py
+++ day_40_CAPSTONE_flight_club/data_manager.py
@@ -4,20 +4,30 @@ from requests import get, put
 SHEET_GUID=env["SH_GUID"]
 SHEET_TOKEN=env["SH_TOKEN"]
 
-API_URL = f"https://api.sheety.co/{SHEET_GUID}/flightDeals/prices"
+API_URL = f"https://api.sheety.co/{SHEET_GUID}/flightDeals"
 HEADERS = {
     "Authorization": f"Basic {SHEET_TOKEN}"
 }
 
 class DataManager:
     @staticmethod
-    def get_sheet_data():
-        req = get(url=API_URL, headers=HEADERS)
+    def get_prices_data():
+        url = f"{API_URL}/prices"
+        req = get(url=url, headers=HEADERS)
         req.raise_for_status()
         return req.json()["prices"]
 
     @staticmethod
-    def update_row_data(body):
-        req =put(f"{API_URL}/{body['id']}", headers=HEADERS, json={"price":body})
+    def update_price_data(body):
+        url = f"{API_URL}/prices"
+        req =put(f"{url}/prices/{body['id']}", headers=HEADERS, json={"price":body})
         req.raise_for_status()
         return req
+
+    @staticmethod
+    def get_users_data():
+        url = f"{API_URL}/users"
+        req = get(url=url, headers=HEADERS)
+        req.raise_for_status()
+        return req.json()["users"]
+
--- day_40_CAPSTONE_flight_club/notification_manager.py
+++ day_40_CAPSTONE_flight_club/notification_manager.py
@@ -1,29 +1,51 @@
 import os
 from twilio.rest import Client
+import smtplib
 
 from flight_data import Flight
 
-# Find your Account SID and Auth Token at twilio.com/console
-# and set the environment variables. See http://twil.io/secure
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-auth_token = os.environ["TWILIO_AUTH_TOKEN"]
-client = Client(account_sid, auth_token)
+TWILIO_ACCOUNT_ID = os.environ["TWILIO_ACCOUNT_SID"]
+TWILIO_AUTH_TOKEN = os.environ["TWILIO_AUTH_TOKEN"]
+
+SMTP_PASS = os.getenv("SMTP_PASS")
+SMTP_MAIL = os.getenv("SMTP_MAIL")
+SMTP_HOST = os.getenv("SMTP_HOST")
+SMTP_PORT = os.getenv("SMTP_PORT")
+
+def make_notification(flight: Flight):
+    return (f"We found a flight for you!\n"
+            f"{flight}")
+
+
+def make_email(first_name, last_name, flight_list):
+    parsed_list = ""
+    for f in flight_list:
+        parsed_list += f"*******\n{f}\n\n"
+    return (f"Hello, {first_name} {last_name}\n"
+            f"We found some cool flights for you:\n\n"
+            f"{parsed_list}")
+
+
+def get_smtp_connection():
+    connection = smtplib.SMTP(host=SMTP_HOST, port=int(SMTP_PORT))
+    connection.starttls()
+    connection.login(user=SMTP_MAIL, password=SMTP_PASS)
+    return connection
+
 
 class NotificationManager:
+    def __init__(self):
+        self.smtp_connection = get_smtp_connection()
+        self.twilio_client = Client(TWILIO_ACCOUNT_ID, TWILIO_AUTH_TOKEN)
 
-    @staticmethod
-    def make_notification(flight: Flight):
-        return (f"Found a flight for you!\n"
-                f"{flight}")
 
-    @staticmethod
-    def send_text_message(body):
-        message = client.messages.create(
+    def send_text_message(self, body):
+        message = self.twilio_client.messages.create(
             to="***REMOVED***",
             from_="***REMOVED***",
             body=body,
         )
         print(message.body)
 
-    
-
+    def send_email(self, to_email, message):
+        self.smtp_connection.sendmail(from_addr=SMTP_MAIL, to_addrs=to_email, msg=f"Subject: Flight Club\n\n{message}")
